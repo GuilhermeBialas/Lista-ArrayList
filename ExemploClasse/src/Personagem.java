591f93208afef3070ab785b6051df61a8c8ec242
GuilhermeBialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-06-08 11:30:41 -0300

    assunto novo

--- ExemploClasse/src/Personagem.java
+++ ExemploClasse/src/Personagem.java
@@ -10,4 +10,24 @@
  */
 public class Personagem {
     
+    private String nome, cla, aldeia ;
+    private long nivelChakra;
+     //permitir guardar a informacao
+    
+    public void setNome(String x1){
+        if(x1.trim().equals("")){
+            throw new 
+                IllegalArgumentException(
+                "Nome não pode ser vazio");
+        }
+    if(x1.trim().length() < 3){
+        throw new IllegalArgumentException(
+        "Nome tem que conter no minimo 3 caracteres");
+    }
+    this.nome = x1;
+    }
+public String getNome(){
+    return nome;
+    
+}
 }
