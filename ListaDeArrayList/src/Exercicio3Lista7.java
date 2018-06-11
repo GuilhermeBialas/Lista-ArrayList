466ccf47e4132092c84d235ab6f31f8b85361aa8
GuilhermeBialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-06-11 08:29:04 -0300

    Finalização do Exercicio03

--- ListaDeArrayList/src/Exercicio3Lista7.java
+++ ListaDeArrayList/src/Exercicio3Lista7.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -9,7 +10,32 @@ import java.util.ArrayList;
 public class Exercicio3Lista7 {
     public static void main(String[] args){
         
-        ArrayList<Character> caracters = new ArrayList<>();
+        ArrayList<Character> caracteres = new ArrayList<>();
+        caracteres.add('B');
+        caracteres.add('i');
+        caracteres.add('a');
+        caracteres.add('l');
+        caracteres.add('h');
+        caracteres.add('i');
+        caracteres.add('n');
+        caracteres.add('h');
+        caracteres.add('a');
+        caracteres.add('s');
+        caracteres.add('L');
+        caracteres.add('e');
+        caracteres.add('g');
+        caracteres.add('a');
+        caracteres.add('l');
+        
+        JOptionPane.showMessageDialog(null,"Os caracteres são: "+ caracteres.get(0)+" "+caracteres.get(1)
+                +" " +caracteres.get(2)+" "+caracteres.get(3)+" "+caracteres.get(4)+" "+caracteres.get(5)
+                +" "+caracteres.get(6)+" "+ caracteres.get(7)+" "+caracteres.get(8)+ " "+caracteres.get(9)+" "+caracteres.get(10)+" "+
+                caracteres.get(11)+" "+ caracteres.get(12)+ " "+caracteres.get(13)+" "+caracteres.get(14));
+        
+        
+        
+        
+        
         
         
         
